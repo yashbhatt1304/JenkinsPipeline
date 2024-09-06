69bb9b90662d014719918294f793a8e10192471d
Yash_Bhatt <yashbhatt1304@gmail.com>
2024-09-06 20:36:16 +0530

    adding executable permissions to file

--- post-script.sh
+++ post-script.sh
@@ -9,3 +9,5 @@ sudo apt install python3-pytest -y
 sudo rm -rf  /etc/nginx/sites-available/default
 sudo cp /home/ubuntu/Jenkins-Pipeline/default /etc/nginx/sites-available/
 sudo systemctl restart nginx
+sudo chmod +x /home/ubuntu/Jenkins-Pipeline/pre-script.sh
+sudo chmod +x /home/ubuntu/Jenkins-Pipeline/post-script.sh
